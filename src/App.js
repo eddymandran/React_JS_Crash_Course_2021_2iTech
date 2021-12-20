5906e18374d29eb868dc1ae2ae489c8a6af4f0d9
eddymandran <eddy.mandran@gmail.com>
2021-12-20 19:03:08 +0100

    creaton du toggle reminder

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./App.css";
 import Tasks from "./components/Tasks";
 
 function App() {
-
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,18 +24,30 @@ function App() {
       day: "Feb 5th at 2:30pm",
       reminder: false,
     },
-  ])
+  ]);
 
-// Delete Task
-const deleteTask = (id) => {
-  setTasks(tasks.filter((task) => task.id !== id))
-}
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : ('No task to show')}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
