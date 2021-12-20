51ff5a5e6b850b8b2be300bfccedee88f7b7754a
eddymandran <eddy.mandran@gmail.com>
2021-12-20 15:09:52 +0100

    Creation composant tasks + affichage rapide des taches

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
 import "./App.css";
+import { Tasks } from "./components/Tasks";
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Tasks/>
     </div>
   );
 }
