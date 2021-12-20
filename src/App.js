1c8c070b03486fa2d56554c682180b9a8be8cd1e
eddymandran <eddy.mandran@gmail.com>
2021-12-20 13:44:21 +0100

    Creation du composant header + mise en place du titre via props et protoTypes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <Header title='Hello' />
     </div>
   );
 }
