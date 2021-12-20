bdfd2d8d2c983d0030a20ceaea83996f7529eaf0
eddymandran <eddy.mandran@gmail.com>
2021-12-20 14:08:28 +0100

    Creation du composant Button + CSS

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header title='Hello' />
+    <div className="container">
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Add'/>
         </header>
     )
 }
@@ -16,4 +18,11 @@ Header.defaultProps = {
 Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
+
+// CSS in JS
+/* const headingStyle = {
+    color: 'red',
+    backgroundColor: "black"
+} */
+
 export default Header
