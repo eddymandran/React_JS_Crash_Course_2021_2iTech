46c413f933e7285a1c7ad8b4a6c8a13d474e2abd
eddymandran <eddy.mandran@gmail.com>
2021-12-21 15:42:40 +0100

    mise en place du routage + creation du about et du footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
@@ -63,7 +66,7 @@ function App() {
       body: JSON.stringify(updTask),
     });
 
-      const data = await res.json()
+    const data = await res.json();
 
     setTasks(
       tasks.map((task) =>
@@ -73,18 +76,35 @@ function App() {
   };
 
   return (
-    <div className="container">
+    <Router>
+    <div className='container'>
       <Header
         onAdd={() => setShowAddTask(!showAddTask)}
         showAdd={showAddTask}
       />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "No tasks to show"
-      )}
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                'No Tasks To Show'
+              )}
+            </>
+          }
+        />
+        <Route path='/about' element={<About />} />
+      </Routes>
+      <Footer />
     </div>
+  </Router>
   );
 }
 
