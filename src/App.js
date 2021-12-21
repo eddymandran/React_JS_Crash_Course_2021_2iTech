7f274194e73015557332bab6b8ce9e404756569c
eddymandran <eddy.mandran@gmail.com>
2021-12-21 11:29:36 +0100

    mise en fonction du bouton permettant d'afficher ou non le formulaire d'ajout + mise en forme conditionnelle

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import "./App.css";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
-
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -30,10 +30,10 @@ function App() {
 
   // Add task
   const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task}
-    setTasks([...tasks, newTask])
-  }
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
   // Delete Task
   const deleteTask = (id) => {
@@ -51,8 +51,11 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
